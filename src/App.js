41a31c59d3815690f05894d969d533518ce6833e
Mohith Pogiri <mohithpogiri@gmail.com>
2024-08-06 23:41:05 +0530

    to-do working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from 'react';
+import './store/store.js';
+import Add from './form.js';
+import Display from './display.js';
+
+import {BrowserRouter as Router, Routes,Route, Link} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+         <Router>
+          <div className='h'>
+          <h1>DEMO on react-redux</h1>
+          <div>
+            <Link to="/display"> <button>Members</button></Link>
+         
+          </div>
+          <div>
+          <Link to="/addmember"> <button>Add</button></Link>
+          </div>
+         
+          </div>
+          <Routes>
+            <Route path="/display" element={<Display/>}/>
+
+            <Route path="/addmember" element={<Add/>}/>
+          </Routes>
+
+
+          </Router>
     </div>
   );
 }
